e66aeeb342f8a47e9d8fde31c7e9f21c8e44e7b5
Jamal Ahmed <jamal.ahmed@student.moringaschool.com>
2024-07-02 12:33:24 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+
+// Function expression called divide
+const divide = function () {
+    return 2000 / 100;
+}
+
+// Arrow function called square
+const square = (num) => {
+    return num * num;
+}
+
+// Arrow function called add
+const add = (a, b) => {
+    return a + b;
+}
